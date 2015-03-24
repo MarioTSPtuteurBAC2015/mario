d95cf4af0e6307425c1d35176a8de08860defeda
cicisong <cecilialiliavico@gmail.com>
2015-03-24 20:28:51 +0100

    Update bibMario.h

--- bibMario.h
+++ bibMario.h
@@ -4,11 +4,16 @@
 #include <SDL/SDL.h>
 #include <math.h>
 
-#define RESX 1200
-#define RESY 720
+#define RESX 1400
+#define RESY 800
 #define TIMER 5
 
-bool estSOL ( SDL_Rect *dstrect );
-void MAJ(Uint8* keys, SDL_Rect *dstrect, float *vX, float *vY, float *t1, float *t2, float *vD);
+void initialisationFenetre ( SDL_Surface** fenetre );
+void lireToucheClavier ( Uint8* toucheClavier );
+void initialisationTimer ( Uint32 *temps );
+void gestionTimer ( Uint32 temps, Uint32 tempsEcoule );
+void effacerFenetre ( SDL_Surface* fenetre );
+void MAJ( Uint8 *toucheClavier, float *pX, float *pY, float *vX, float *vY, float *tempsDec, float *tempsAcc, float *vD);
 
 #endif
+
