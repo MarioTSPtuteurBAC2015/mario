1cc84d170a11372f30ffb4242503a916d2eb79bb
totallyhs <heescool@gmail.com>
2015-05-12 14:06:09 +0200

    Create jeu.h

---
+++ jeu.h
@@ -0,0 +1,12 @@
+#ifndef JEU_H_INCLUDED
+#define JEU_H_INCLUDED
+
+/**
+*\fn jouer
+*\brief lancer le jeu
+*\param [in] fenetre Pointeur sur la fenêtre à s'afficher
+**/
+
+void jouer ( SDL_Surface* fenetre );
+
+#endif // JEU_H_INCLUDED
