63b3fb31473ab467e11ca4bc77ba7d25a638d22f
cicisong <cecilialiliavico@gmail.com>
2015-05-14 17:32:11 +0200

    Update jeu.cpp
    correction de quelques bugs

--- jeu.cpp
+++ jeu.cpp
@@ -1,5 +1,3 @@
-/** \file jeu.c **/
-
 #ifdef __cplusplus
     #include <cstdlib>
 #else
@@ -11,38 +9,43 @@
 
 void jouer ( SDL_Surface *fenetre )
 {
+    printf("jeu");
     Uint32 temps, tempsEcoule;
+
     int numkeys;
     Uint8 *toucheClavier;
     float positionX = 3*RESX/4, positionY = 3*RESY/4;
     float vitesseX = 0, vitesseY = 0;
     float tempsDeceleration = 0, tempsAcceleration = 0;
     float vitesseDeceleration = 0;
-    SDL_Rect* mur = NULL;
-    SDL_Rect* perso = NULL;
-    SDL_Rect *sol = NULL;
+    SDL_Rect mur;
+    SDL_Rect perso;
+    SDL_Rect sol;
 
-    sol->x = 0;
-    sol->y = 0;
-    mur->x = 0;
-    mur->y = 0;
+    sol.x = 0;
+    sol.y = 0;
+    mur.x = 0;
+    mur.y = 0;
+    perso.x = 50;
+    perso.y = 50;
 
+    SDL_Surface* bmp = SDL_LoadBMP("mario.bmp");
 
-    SDL_Surface* bmp = SDL_LoadBMP("cb.bmp");
+    SDL_BlitSurface(bmp, 0, fenetre, &perso);
+
+    SDL_Flip(fenetre);
 
     SDL_Rect dstrect;
 
     toucheClavier = SDL_GetKeyState(&numkeys);
 
     while ( !toucheClavier[SDLK_ESCAPE] ){
-
         initialisationTimer ( &temps );
 
-        MAJ( toucheClavier, &positionX, &positionY, &vitesseX, &vitesseY, &tempsDeceleration, &tempsAcceleration, &vitesseDeceleration, mur, perso, sol);
+        MAJ( toucheClavier, &positionX, &positionY, &vitesseX, &vitesseY, &tempsDeceleration, &tempsAcceleration, &vitesseDeceleration, &mur, &perso, &sol);
 
         dstrect.x = (int) positionX;
         dstrect.y = (int) positionY;
-
         effacerFenetre ( fenetre );
 
         SDL_BlitSurface(bmp, 0, fenetre, &dstrect);
@@ -53,4 +56,5 @@ void jouer ( SDL_Surface *fenetre )
     }
 
     SDL_FreeSurface(bmp);
+
 }
