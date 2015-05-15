e4693e9cb49b45196fdf2ea0fb7389f38afddb19
cicisong <cecilialiliavico@gmail.com>
2015-05-16 00:44:35 +0200

    Update fmap.h

--- fmap.h
+++ fmap.h
@@ -1,39 +1,12 @@
-#include <sdl/sdl.h>
+#ifndef FMAP_H
+#define FMAP_H
+#include <SDL/SDL.h>
+#include "structures.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif // __cplusplus
-
-/**
- * \struct TileProp fmap.h
- * \brief les rectangles de tileset et le personnage peut y promener si pas mur
- */
- typedef struct
-{
-	SDL_Rect R;
-	int mur;
-	// tout ce que vous voulez...
-} TileProp;
-
-/**
- *\struct Map fmap.h
- *\brief la structure map qui permet de définir le map et le scrolling
- */
-typedef struct
-{
-	int LARGEUR_TILE,HAUTEUR_TILE;
-	int nbtilesX,nbtilesY;
-	SDL_Surface* tileset;
-	TileProp* props;
-	Uint16** schema;
-	int nbtiles_largeur_monde,nbtiles_hauteur_monde;
-	int xscroll,yscroll;
-	int largeur_fenetre,hauteur_fenetre;
-} Map;
 
 /**
  *\fn ChargerMap fic
- *\param [in] fic le fichier texte où les propriétés du map sont sauvegardés
+ *\param [in] fic le fichier texte où les propriétés du map sont sauvegardées
  *\param [in] largeur_fenetre
  *\param [in] hauteur_fenetre
  *\param [out] Map
@@ -44,17 +17,22 @@ Map* ChargerMap(const char* fic,int largeur_fenetre,int hauteur_fenetre);
  *\fn AfficherMap
  *\param [in] m Pointeur sur Map
  *\param [in] screen Pointeur sur SDL_Surface
- *\param [out] 0 si le map est bien chargé
+ *\param [out] 0 si la map est bien chargée
  */
 int AfficherMap(Map* m,SDL_Surface* screen);
 
 /**
  *\fn LibererMap
  *\param [in] m Pointeur sur Map
- *\param [out] 0 si le map a été bien libéré dans l'espace mémoire
+ *\param [out] 0 si la map a été bien libérée dans l'espace mémoire
  */
 int LibererMap(Map* m);
 
-#ifdef __cplusplus
-}
-#endif // __cplusplus
+/**
+* @brief fait bouger la map
+* @param m La map sur laquelle on travaille
+* @param in
+*/
+void moveMap(Map *m,Uint8* toucheClavier,float* vX,float* vY);
+
+#endif
